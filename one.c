77fc35d7e3dfbd6ebb5b4d68ba4db89699f9caab
Wilfred Hughes <me@wilfred.me.uk>
2014-06-28 13:27:57 +0100

    Adding a minimal C program.

---
+++ one.c
@@ -0,0 +1,12 @@
+// An outrageously simple C program that we hope to compile with Centric.
+//
+// To test with GCC:
+//   $ gcc -Wall one.c -o one
+//   $ ./one
+//   $ echo $?
+//   1
+#include <stdio.h>
+
+int main() {
+    return 1;
+}
