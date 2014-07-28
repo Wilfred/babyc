5de6c88597f88fec9ded15b050c087dceb13910f
Wilfred Hughes <me@wilfred.me.uk>
2014-07-28 21:01:35 +0100

    Removing comments from our simplest unit test, so it doesn't depend on comment parsing.

--- test_programs/immediate__return_1.c
+++ test_programs/immediate__return_1.c
@@ -1,10 +1,3 @@
-// An outrageously simple C program that we hope to compile with Babyc.
-//
-// To test with GCC:
-//   $ gcc -Wall one.c -o one
-//   $ ./one
-//   $ echo $?
-//   1
 int main() {
     return 1;
 }
